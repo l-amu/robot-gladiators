178fb5f3f9eec441ad5804a6dc0fda6ef7f0e76e
l-amu <lamiekgim@gmail.com>
2021-09-12 13:53:01 -0400

    recent

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,20 @@
-window.alert("This is an alert! JavaScript is running!");
+var palyerName = window.prompt("What is your robot's name");
+var playerHealth = 100;
+var playerAttack = 10;
+
+// You can also log multiple values at once like this
+// console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roberto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+    window.alert("Welcome to Robert Gladiators!");
+};
+
+fight();
+enemyHealth = enemyHealth - playerAttack;
+console.log(
+    playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+);
