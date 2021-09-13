9c8355f81d7916030d8cc34f7b6bb023dab5962d
l-amu <lamiekgim@gmail.com>
2021-09-13 18:27:09 -0400

    added more value

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,4 +1,4 @@
-var palyerName = window.prompt("What is your robot's name");
+var playerName = window.prompt("What is your robot's name");
 var playerHealth = 100;
 var playerAttack = 10;
 
@@ -18,3 +18,22 @@ enemyHealth = enemyHealth - playerAttack;
 console.log(
     playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
 );
+
+if (enemyHealth <= 0) {
+    window.alert(enemyName + "has died!");
+}
+else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+}
+playerHealth = playerHealth - enemyAttack;
+console.log(
+    enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+  );
+
+  if (playerHealth <= 0) {
+    window.alert(playerName + " has died!");
+  } 
+  else {
+    window.alert(playerName + " still has " + playerHealth + " health left.");
+  }
+
