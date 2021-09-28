ebcafbafdd237a9ddc9573e4449b39e37827b517
l-amu <lamiekgim@gmail.com>
2021-09-28 01:57:04 -0400

    add endgame function

--- assets/js/game.js
+++ assets/js/game.js
@@ -73,7 +73,11 @@ var fight = function(enemyName) {
     }
   }
 };
+
 var startGame = function () {
+  playerHealth = 100;
+  playerAttack = 10;
+  playerMoney = 10;
   for (var i = 0; i < enemyNames.length; i++) {
     if (playerHealth > 0) {
       window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
@@ -90,21 +94,37 @@ var startGame = function () {
 
 
     // debugger;
-    startGame();
+    // startGame();
+    endGame();
   }
 };
 //should run the fight function, and pass in "roborto"
-fight(enemyNames[0]);
+// fight(enemyNames[0]);
+
+var endGame = function() {
+  if (playerHealth > 0) {
+    window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+  } 
+  else {
+    window.alert("You've lost your robot in battle.");
+  }
+  var playAgainConfirm = window.confirm("Would you like to play again?");
+
+if (playAgainConfirm) {
+  startGame();
+} 
+else {
+  window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+}
+};
+
+
+
+
+
+
+
 startGame();
-
-
-
-
-
-
-
-
-
 
 
 
