dbfa36e607035f19920544aeba3900ca8b58069d
l-amu <lamiekgim@gmail.com>
2021-09-29 00:49:08 -0400

    added endgame and start gaem function

--- assets/js/game.js
+++ assets/js/game.js
@@ -18,7 +18,7 @@ console.log(1);
 
 
 
-var fight = function(enemyName) {
+var fight = function (enemyName) {
   while (playerHealth > 0 && enemyHealth > 0) {
     // ask player if they'd like to fight or run
     var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
@@ -87,6 +87,15 @@ var startGame = function () {
       enemyHealth = 50;
 
       fight(pickedEnemyName);
+      if (playerHealth > 0 && i < enemyNames.length - 1) {
+
+        var storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
+
+        if (storeConfirm) {
+          shop();
+        }
+        
+      }
     } else {
       window.alert("You have lost your robot in battle! Game Over!");
       break;
@@ -101,24 +110,74 @@ var startGame = function () {
 //should run the fight function, and pass in "roborto"
 // fight(enemyNames[0]);
 
-var endGame = function() {
+var endGame = function () {
   debugger;
   if (playerHealth > 0) {
     window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
-  } 
+  }
   else {
     window.alert("You've lost your robot in battle.");
   }
   var playAgainConfirm = window.confirm("Would you like to play again?");
 
-if (playAgainConfirm) {
-  // this was startGame but I changed it to gight because it ruined the loop
-  // startGame();
-  fight();
-} 
-else {
-  window.alert("Thank you for playing Robot Gladiators! Come back soon!");
-}
+  if (playAgainConfirm) {
+    // this was startGame but I changed it to gight because it ruined the loop
+    // startGame();
+    fight();
+  }
+  else {
+    window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+  }
+};
+
+var shop = function () {
+  var shopOptionPrompt = window.prompt(
+    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+  );
+  switch (shopOptionPrompt) {
+    case "REFILL": // new case
+    case "refill":
+      if (playerMoney >= 7) {
+      window.alert("Refilling player's health by 20 for 7 dollars.");
+  
+      // increase health and decrease money
+      playerHealth = playerHealth + 20;
+      playerMoney = playerMoney - 7;
+      }
+      else {
+        window.alert("You don't have enough money!");
+      }
+      break;
+    
+    case "UPGRADE": // new case  
+    case "upgrade":
+      if (playerMoney >= 7) {
+
+      window.alert("Upgrading player's attack by 6 for 7 dollars.");
+  
+      // increase attack and decrease money
+      playerAttack = playerAttack + 6;
+      playerMoney = playerMoney - 7;
+      }
+      else {
+        window.alert("You don't have enough money!");
+
+      }
+      break;
+    case "leave":
+      window.alert("Leaving the store.");
+  
+      // do nothing, so function will end
+      break;
+    default:
+      window.alert("You did not pick a valid option. Try again.");
+  
+      // call shop() again to force player to pick a valid option
+      shop();
+      break;
+  }
+
+
 };
 
 
