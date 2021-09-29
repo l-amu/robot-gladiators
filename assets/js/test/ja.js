b695f78981bf2ab453f3e7befaed13a04949d7a2
l-amu <lamiekgim@gmail.com>
2021-09-29 18:30:15 -0400

    random health attack values

---
+++ assets/js/test/ja.js
@@ -0,0 +1,8 @@
+console.log(Math);// prints 3.141592653589793
+console.log(Math.PI);
+
+ // rounds to the nearest whole number (4)
+console.log(Math.round(4.4));
+
+ // prints the square root (5)
+console.log(Math.sqrt(25));
